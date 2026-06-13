26a1648b3f6c12f94a13f4e942f9403e60c8cd9a
akaInvincible <akashagarwal1321@gmail.com>
2026-06-13 21:07:58 +0530

    Added findRestaurantStrategy

---
+++ ProcessingCapacityRestaurantStrategy.js
@@ -0,0 +1,27 @@
+class ProcessingCapacityRestaurantStrategy {
+    findRestaurant(availableRestaurants, menuListItems) {
+        if (!availableRestaurants || availableRestaurants.length === 0) return null;
+
+        let selectedRestaurant = null;
+        let maxCapacity = -1;
+
+        for (let restaurant of availableRestaurants) {
+
+            let hasAllItems = true;
+
+            for (let item of menuListItems) {
+                if (!restaurant.getMenuItems().has(item)) {
+                    hasAllItems = false;
+                    break;
+                }
+            }
+
+            if (hasAllItems && restaurant.getProcessingCapacity() > maxCapacity) {
+                maxCapacity = restaurant.getProcessingCapacity();
+                selectedRestaurant = restaurant;
+            }
+        }
+
+        return selectedRestaurant;
+    }
+}
